24d439efcaa16fae29eaff2c1ebed794d0232899
Krzysztof Sajkowski <ksajkowski@mcx.pl>
2020-11-20 22:45:02 +0100

    Initial commit

---
+++ src/test/java/pl/fixit/starogardo/common/Starogardocommon/StarogardoCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fixit.starogardo.common.Starogardocommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarogardoCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
